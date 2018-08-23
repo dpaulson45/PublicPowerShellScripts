1908ebfd7de3248271790ee648a161d9891d1634
David Paulson <dpaul@microsoft.com>
2018-08-23 18:24:51 -0500

    Added Get-ServerOperatingSystemVersion Function
    Added the Get-ServerOperatingSystemVersion Function script

---
+++ Functions/Get-ServerOperatingSystemVersion/Get-ServerOperatingSystemVersion.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding()]
+param(
+[string]$OSBuildNumberVersion
+)
+
+if($OSBuildNumberVersion -eq [string]::Empty -or $OSBuildNumberVersion -eq $null)
+{
+    Write-Verbose("Getting the local machine version build number")
+    $OSBuildNumberVersion = (Get-WmiObject -Class Win32_OperatingSystem).Version
+    Write-Verbose("Got {0} for the version build number" -f $OSBuildNumberVersion)
+}
+else 
+{
+    Write-Verbose("Passed - [string]OSBuildNumberVersion : {0}" -f $OSBuildNumberVersion)
+}
+
+$osReturnValue
+switch ($OSBuildNumberVersion) 
+{
+    "6.0.6000" {$osReturnValue = "Windows2008"}
+    "6.1.7600" {$osReturnValue = "Windows2008R2"}
+    "6.1.7601" {$osReturnValue = "Windows2008R2"}
+    "6.2.9200" {$osReturnValue = "Windows2012"}
+    "6.3.9600" {$osReturnValue = "Windows2012R2"}
+    "10.0.14393" {$osReturnValue = "Windows2016"}
+    "10.0.17713" {$osReturnValue = "Windows2019"}
+    default {$osReturnValue = "Unknown"}
+}
+
+Write-Verbose("Returned: {0}" -f $osReturnValue)
+return $osReturnValue
---
+++ Functions/Get-ServerOperatingSystemVersion/README.md
@@ -0,0 +1,2 @@
+# Get-ServerOperatingSystemVersion
+Function that is used to be able to return a string of what your Server OS is on. 
