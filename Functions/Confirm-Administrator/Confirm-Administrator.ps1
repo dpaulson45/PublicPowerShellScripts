3e2de5bb514e0897591957a555853e911c55103c
David Paulson <dpaul@microsoft.com>
2018-08-23 18:36:28 -0500

    Added Confirm-Administrator
    Added Confirm-Administrator Function

---
+++ Functions/Confirm-Administrator/Confirm-Administrator.ps1
@@ -0,0 +1,9 @@
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
+if($currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator ))
+{
+    return $true 
+}
+else 
+{
+    return $false 
+}
---
+++ Functions/Confirm-Administrator/README.md
@@ -0,0 +1,2 @@
+# Confirm-Administrator
+Function that is used to check to see if your PowerShell script is running as an Admin or not
