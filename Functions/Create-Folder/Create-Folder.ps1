0631575e55cb4841a2802d84d6b0eacd932dd5e1
David Paulson <dpaul@microsoft.com>
2018-08-30 13:01:12 -0500

    Added Create-Folder Function

---
+++ Functions/Create-Folder/Create-Folder.ps1
@@ -0,0 +1,61 @@
+[CmdletBinding()]
+param(
+[Parameter(Mandatory=$true)][string]$NewFolder,
+[Parameter(Mandatory=$false)][bool]$IncludeDisplayCreate,
+[Parameter(Mandatory=$false)][scriptblock]$VerboseFunctionCaller,
+[Parameter(Mandatory=$false)][scriptblock]$HostFunctionCaller
+)
+
+
+Function Write-VerboseWriter {
+    param(
+    [Parameter(Mandatory=$true)][string]$WriteString 
+    )
+        if($VerboseFunctionCaller -eq $null)
+        {
+            Write-Verbose $WriteString
+        }
+        else 
+        {
+            &$VerboseFunctionCaller $WriteString
+        }
+    }
+    
+    Function Write-HostWriter {
+    param(
+    [Parameter(Mandatory=$true)][string]$WriteString 
+    )
+        if($HostFunctionCaller -eq $null)
+        {
+            Write-Host $WriteString
+        }
+        else
+        {
+            &$HostFunctionCaller $WriteString    
+        }
+    }
+$passedVerboseFunctionCaller = $false
+$passedHostFunctionCaller = $false
+if($VerboseFunctionCaller -ne $null){$passedVerboseFunctionCaller = $true}
+if($HostFunctionCaller -ne $null){$passedHostFunctionCaller = $true}
+Write-VerboseWriter("Calling: Create-Folder")
+Write-VerboseWriter("Passed: [string]NewFolder: {0} | [bool]IncludeDisplayCreate: {1} | [scriptblock]VerboseFunctionCaller: {2} | [scriptblock]HostFunctionCaller: {3}" -f $NewFolder,
+$IncludeDisplayCreate,
+$passedVerboseFunctionCaller,
+$passedHostFunctionCaller)
+
+if(-not (Test-Path -Path $NewFolder))
+{
+    if($IncludeDisplayCreate)
+    {
+        Write-HostWriter("Creating Directory: {0}" -f $NewFolder)
+    }
+    [System.IO.Directory]::CreateDirectory($NewFolder) | Out-Null
+}
+else 
+{
+    if($IncludeDisplayCreate)
+    {
+        Write-HostWriter("Directory {1} is already created!" -f $NewFolder)
+    }
+}
