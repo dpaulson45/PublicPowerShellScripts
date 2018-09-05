2d13f15941e5c0332aff0f70ec467ef1d5c1040b
David Paulson <dpaul@microsoft.com>
2018-09-04 19:39:16 -0500

    Added Enter-YesNoLoopAction

---
+++ Functions/Enter-YesNoLoopAction/Enter-YesNoLoopAction.ps1
@@ -0,0 +1,42 @@
+[CmdletBinding()]
+param(
+[Parameter(Mandatory=$true)][string]$Question,
+[Parameter(Mandatory=$true)][scriptblock]$YesAction,
+[Parameter(Mandatory=$true)][scriptblock]$NoAction,
+[Parameter(Mandatory=$false)][scriptblock]$VerboseFunctionCaller
+)
+
+#Function Version 1.0
+Function Write-VerboseWriter {
+    param(
+    [Parameter(Mandatory=$true)][string]$WriteString 
+    )
+        if($VerboseFunctionCaller -eq $null)
+        {
+            Write-Verbose $WriteString
+        }
+        else 
+        {
+            &$VerboseFunctionCaller $WriteString
+        }
+    }
+    
+$passedVerboseFunctionCaller = $false
+if($VerboseFunctionCaller -ne $null){$passedVerboseFunctionCaller = $true}
+Write-VerboseWriter("Calling: Enter-YesNoLoopAction")
+Write-VerboseWriter("Passed: [string]Question: {0} | [bool]VerboseFunctionCaller: {1}" -f $Question, 
+$passedVerboseFunctionCaller)
+
+do{
+    $answer = Read-Host ("{0} 'y' or 'n'" -f $Question)
+    Write-VerboseWriter("Read-Host answer: {0}" -f $answer)
+}while($answer -ne 'n' -and $answer -ne 'y')
+
+if($answer -eq 'y')
+{
+    &$YesAction
+}
+else 
+{
+    &$NoAction
+}
---
+++ Functions/Enter-YesNoLoopAction/README.md
@@ -0,0 +1,2 @@
+# Enter-YesNoLoopAction
+Function that is used to check to loop you through a question waiting for a 'y' or 'no' response and take action on it based off the function that is passed to it. 
