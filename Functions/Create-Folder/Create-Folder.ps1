b3d6cbead48073358f55ceb461e4266aaa3d58bf
David Paulson <dpaul@microsoft.com>
2018-09-10 11:06:35 -0500

    added the ability to be called within an Invoke-Command and return the verbose and write data

--- Functions/Create-Folder/Create-Folder.ps1
+++ Functions/Create-Folder/Create-Folder.ps1
@@ -1,17 +1,24 @@
 [CmdletBinding()]
 param(
-[Parameter(Mandatory=$true)][string]$NewFolder,
+[Parameter(Mandatory=$false)][string]$NewFolder,
 [Parameter(Mandatory=$false)][bool]$IncludeDisplayCreate,
+[Parameter(Mandatory=$false)][bool]$InvokeCommandReturnWriteArray,
+[Parameter(Mandatory=$false,Position=1)][object]$PassedParametersObject,
 [Parameter(Mandatory=$false)][scriptblock]$VerboseFunctionCaller,
 [Parameter(Mandatory=$false)][scriptblock]$HostFunctionCaller
 )
 
-#Function Version 1.0
+#Function Version 1.1
 Function Write-VerboseWriter {
     param(
     [Parameter(Mandatory=$true)][string]$WriteString 
     )
-        if($VerboseFunctionCaller -eq $null)
+        if($InvokeCommandReturnWriteArray)
+        {
+            $hashTable = @{"Verbose"=("[Remote Server: {0}] : {1}" -f $env:COMPUTERNAME, $WriteString)}
+            Set-Variable stringArray -Value ($stringArray += $hashTable) -Scope 1 
+        }
+        elseif($VerboseFunctionCaller -eq $null)
         {
             Write-Verbose $WriteString
         }
@@ -25,7 +32,12 @@ Function Write-VerboseWriter {
     param(
     [Parameter(Mandatory=$true)][string]$WriteString 
     )
-        if($HostFunctionCaller -eq $null)
+        if($InvokeCommandReturnWriteArray)
+        {
+            $hashTable = @{"Host"=("[Remote Server: {0}] : {1}" -f $env:COMPUTERNAME, $WriteString)}
+            Set-Variable stringArray -Value ($stringArray += $hashTable) -Scope 1 
+        }
+        elseif($HostFunctionCaller -eq $null)
         {
             Write-Host $WriteString
         }
@@ -36,17 +48,27 @@ Function Write-VerboseWriter {
     }
 $passedVerboseFunctionCaller = $false
 $passedHostFunctionCaller = $false
+$passedPassedParametersObject = $false
 if($VerboseFunctionCaller -ne $null){$passedVerboseFunctionCaller = $true}
 if($HostFunctionCaller -ne $null){$passedHostFunctionCaller = $true}
+if($passedPassedParametersObject -ne $null){$passedPassedParametersObject = $true}
+$stringArray = @() 
+if($PassedParametersObject -ne $null)
+{
+    $NewFolder = $PassedParametersObject.NewFolder 
+    $InvokeCommandReturnWriteArray = $true 
+}
 Write-VerboseWriter("Calling: Create-Folder")
-Write-VerboseWriter("Passed: [string]NewFolder: {0} | [bool]IncludeDisplayCreate: {1} | [scriptblock]VerboseFunctionCaller: {2} | [scriptblock]HostFunctionCaller: {3}" -f $NewFolder,
+Write-VerboseWriter("Passed: [string]NewFolder: {0} | [bool]IncludeDisplayCreate: {1} | [bool]InvokeCommandReturnWriteArray: {2} | [object]PassedParametersObject: {3} | [scriptblock]VerboseFunctionCaller: {4} | [scriptblock]HostFunctionCaller: {5}" -f $NewFolder,
 $IncludeDisplayCreate,
+$InvokeCommandReturnWriteArray,
+$passedPassedParametersObject,
 $passedVerboseFunctionCaller,
 $passedHostFunctionCaller)
 
 if(-not (Test-Path -Path $NewFolder))
 {
-    if($IncludeDisplayCreate)
+    if($IncludeDisplayCreate -or $InvokeCommandReturnWriteArray)
     {
         Write-HostWriter("Creating Directory: {0}" -f $NewFolder)
     }
@@ -54,8 +76,12 @@ if(-not (Test-Path -Path $NewFolder))
 }
 else 
 {
-    if($IncludeDisplayCreate)
+    if($IncludeDisplayCreate -or $InvokeCommandReturnWriteArray)
     {
         Write-HostWriter("Directory {0} is already created!" -f $NewFolder)
     }
+}
+if($InvokeCommandReturnWriteArray)
+{
+    return $stringArray
 }
