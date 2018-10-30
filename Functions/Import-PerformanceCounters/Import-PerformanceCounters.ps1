be3c5ad205636dcd5dbad094eeb4a3998be556cc
David Paulson <dpaul@microsoft.com>
2018-10-29 21:34:18 -0500

    Updated Get-CounterSampleToCounterObjectName Master Template

--- Functions/Import-PerformanceCounters/Import-PerformanceCounters.ps1
+++ Functions/Import-PerformanceCounters/Import-PerformanceCounters.ps1
@@ -43,25 +43,9 @@ param(
 [Parameter(Mandatory=$true)][object]$PerformanceCounterSample
 )
 
-#Function Version 1.0
+#Function Version 1.1
 
-Add-Type @"
-
-namespace PerformanceCounters
-{
-    public class PerformanceCounterName
-    {
-        public string FullName; 
-        public string ServerName; 
-        public string ObjectName; 
-        public string InstanceName; 
-        public string CounterName; 
-    }
-}
-
-"@
-
-$counterObj = New-Object -TypeName PerformanceCounters.PerformanceCounterName
+$counterObj = New-Object -TypeName pscustomobject
 $FullName = $PerformanceCounterSample.Path 
 #\\adt-e2k13aio1\logicaldisk(harddiskvolume1)\avg. disk sec/read
 $endOfServerIndex = $FullName.IndexOf("\",2) #\\adt-e2k13aio1 <> \logicaldisk(harddiskvolume1)\avg. disk sec/read
@@ -76,11 +60,11 @@ else
 {
     $instanceName = ($PerformanceCounterSample.InstanceName)
 }
-$counterObj.FullName = $FullName
-$counterObj.ServerName = ($FullName.Substring(2,($endOfServerIndex - 2)))
-$counterObj.ObjectName = ($FullName.Substring($endOfServerIndex + 1, $endOfCounterObjectIndex - $endOfServerIndex - 1))
-$counterObj.InstanceName = $instanceName
-$counterObj.CounterName = ($FullName.Substring($startOfCounterIndex))
+$counterObj | Add-Member -MemberType NoteProperty -Name "FullName" -Value $FullName
+$counterObj | Add-Member -MemberType NoteProperty -Name "ServerName" -Value ($FullName.Substring(2,($endOfServerIndex - 2)))
+$counterObj | Add-Member -MemberType NoteProperty -Name "ObjectName" -Value ($FullName.Substring($endOfServerIndex + 1, $endOfCounterObjectIndex - $endOfServerIndex - 1))
+$counterObj | Add-Member -MemberType NoteProperty -Name "InstanceName" -Value $instanceName
+$counterObj | Add-Member -MemberType NoteProperty -Name "CounterName" -Value ($FullName.Substring($startOfCounterIndex))
 
 return $counterObj
 }
