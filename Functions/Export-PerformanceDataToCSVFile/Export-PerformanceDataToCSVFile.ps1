f6922839f2efa8f3b07158234b79b94574643c0c
David Paulson <dpaul@microsoft.com>
2018-11-01 17:55:01 -0500

    Added Export-PerformanceDataToCSVFile Function

---
+++ Functions/Export-PerformanceDataToCSVFile/Export-PerformanceDataToCSVFile.ps1
@@ -0,0 +1,72 @@
+[CmdletBinding()]
+param(
+[Parameter(Mandatory=$true)][hashtable]$LoadedPerfObject,
+[Parameter(Mandatory=$true)][string]$FullSavePath
+)
+
+#Function Version 1.0
+#[hashtable]LoadedPerfObject - return object from Load-PerformanceCounters:  https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Import-PerformanceCounters/Import-PerformanceCounters.ps1
+########################
+#
+# Write Functions 
+#
+########################
+
+Function Write-VerboseWriter {
+param(
+[Parameter(Mandatory=$true)][string]$WriteString 
+)
+    if($VerboseFunctionCaller -eq $null)
+    {
+        Write-Verbose $WriteString
+    }
+    else 
+    {
+        &$VerboseFunctionCaller $WriteString
+    }
+}
+    
+Function Write-HostWriter {
+param(
+[Parameter(Mandatory=$true)][string]$WriteString 
+)
+    if($HostFunctionCaller -eq $null)
+    {
+        Write-Host $WriteString
+    }
+    else
+    {
+        &$HostFunctionCaller $WriteString    
+    }
+}
+
+########## Parameter Binding Exceptions ##############
+if(Test-Path $FullSavePath)
+{
+    $throwString = "This file already exists '{0}'" -f $FullSavePath
+    throw [System.Management.Automation.ParameterBindingException] $throwString
+}
+
+$firstKey = ($LoadedPerfObject.GetEnumerator() | Select-Object -First 1).Name
+$masterCount = $LoadedPerfObject[$firstKey].AllData.Count 
+[System.Collections.Generic.List[System.Object]]$timeList = New-Object -TypeName System.Collections.Generic.List[System.Object]
+$index = 0
+while($index -lt $masterCount)
+{
+    $addTimer = $true 
+    $timeFrameObject = New-Object pscustomobject 
+
+    foreach($key in $LoadedPerfObject.Keys)
+    {
+        if($addTimer)
+        {
+            $timeFrameObject | Add-Member -MemberType NoteProperty -Name "TimeStamp" -Value ($LoadedPerfObject[$key].AllData[$index].TimeStamp)
+            $addTimer = $false 
+        }
+        $timeFrameObject | Add-Member -MemberType NoteProperty -Name $key -Value ($LoadedPerfObject[$key].AllData[$index].CookedValue)
+    }
+    $timeList.Add($timeFrameObject)
+    $index++
+}
+
+$timeList | Export-Csv $FullSavePath
---
+++ Functions/Export-PerformanceDataToCSVFile/README.md
@@ -0,0 +1,2 @@
+# Export-PerformanceDataToCSVFile
+Function that is used to save out the performance data to a csv file that you might have manipulated for later review. 
