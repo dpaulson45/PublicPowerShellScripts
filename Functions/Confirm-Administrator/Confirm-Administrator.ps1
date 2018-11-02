6e16cc06d725cf71aa00fbef08362bcaba320329
David Paulson <dpaul@microsoft.com>
2018-11-01 22:46:05 -0500

    Update script version
    to make it easy for creating pester test units

--- Functions/Confirm-Administrator/Confirm-Administrator.ps1
+++ Functions/Confirm-Administrator/Confirm-Administrator.ps1
@@ -1,10 +1,12 @@
-#Function Version 1.0
-$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
-if($currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator ))
-{
-    return $true 
-}
-else 
-{
-    return $false 
+Function Confirm-Administrator {
+    #Function Version 1.1
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
+    if($currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator ))
+    {
+        return $true 
+    }
+    else 
+    {
+        return $false 
+    }
 }
