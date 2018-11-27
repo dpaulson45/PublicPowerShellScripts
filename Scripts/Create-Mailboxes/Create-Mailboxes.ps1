8dce993ba23aa8eebc8ea093a4599ea432690926
David Paulson <dpaul@microsoft.com>
2018-11-27 10:46:36 -0600

    Added Create-Mailboxes Script
    Bulk script to make mailboxes for a lab.

---
+++ Scripts/Create-Mailboxes/Create-Mailboxes.ps1
@@ -0,0 +1,50 @@
+param(
+[string]$BaseName="Zelda",
+[int]$NumberOfUsers=100,
+[int]$Padding = 2,
+[int]$StartingNumber = 1,
+[Parameter(Mandatory=$true)][string]$Password, 
+[Parameter(Mandatory=$false)][string]$DomainName = (Get-ADDomain -Current LocalComputer).DNSRoot,
+[Parameter(Mandatory=$true)][Array]$MailboxDatabases,
+[Parameter(Mandatory=$false)][string]$OrganizationalUnit
+)
+
+$securepassword = (ConvertTo-SecureString -AsPlainText -String $Password -Force)
+#RandomObject 
+$randobj = New-Object System.Random
+
+Function Get-RandomDatabase()
+{
+    $mbd = $null
+    $index = $randobj.Next(0,$MailboxDatabases.Count)
+    $mbd = $MailboxDatabases[$index]
+    return $mbd
+}
+
+Function Main 
+{
+   
+    for($i = 1; $i -le $NumberOfUsers; $i++)
+    {
+        $UserName = "{0}{1}" -f $BaseName, $StartingNumber.ToString().PadLeft($Padding,"0")
+        $StartingNumber++
+        $upn = $UserName + "@" + $DomainName
+        $db = Get-RandomDatabase
+        $params = @{
+        Name = $UserName
+        UserPrincipalName = $upn
+        SamAccountName = $UserName
+        Password = $securepassword 
+        ResetPasswordOnNextLogon = $false 
+        Database = $db
+        }
+        if($OrganizationalUnit -ne $null -or $OrganizationalUnit -ne [string]::Empty)
+        {
+            $params.Add("OrganizationalUnit", $OrganizationalUnit)
+        }
+        New-Mailbox @params
+    }
+
+}
+
+Main 
---
+++ Scripts/Create-Mailboxes/README.md
@@ -0,0 +1,2 @@
+# Create-Mailboxes
+Script that is used to create a bunch of mailboxes for a lab environment.
