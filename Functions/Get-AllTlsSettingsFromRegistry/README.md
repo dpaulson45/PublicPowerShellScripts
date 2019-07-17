9ee3a05b7e76007d9217d5601e668510a1c2c475
David Paulson <dpaul@microsoft.com>
2019-07-17 17:05:54 -0500

    Added Get-AllTlsSettingsFromRegistry function
    Still need to add testing scenario for this function, however the code is ready for use.

---
+++ Functions/Get-AllTlsSettingsFromRegistry/README.md
@@ -0,0 +1,2 @@
+# Get-AllTlsSettingsFromRegistry
+Function that is used to get all the required TLS settings from the server to determine how it is configured. 
---
+++ Functions/Get-AllTlsSettingsFromRegistry/Tests/Get-AllTlsSettingsFromRegistry.Tests.ps1
@@ -0,0 +1,30 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.','.'
+$here = $here.Replace("\Tests","")
+. "$here\$sut"
+
+#Load Required Functions
+
+$content = Get-Content "$here\$sut" 
+$foundRequiredFunctions = $false 
+foreach($line in $content)
+{
+    if($foundRequiredFunctions)
+    {
+        if($line.Contains("#>"))
+        {
+            break 
+        }
+        $webRequest = Invoke-WebRequest $line.Trim()
+        if($webRequest -ne $null -and $webRequest.Content)
+        {
+            Invoke-Expression $webRequest.Content 
+        }
+    }
+    if($line.Contains("Required Functions:"))
+    {
+        $foundRequiredFunctions = $true 
+        continue
+    }
+}
+
