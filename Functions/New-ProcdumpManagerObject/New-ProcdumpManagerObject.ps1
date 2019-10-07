120ff2eac237dac3728f42e994615857e3cbba75
David Paulson <dpaul@microsoft.com>
2019-10-07 13:01:40 -0500

    Created New-ProcdumpManagerObject
    Intial creation of New-ProcdumpManagerObject

---
+++ Functions/New-ProcdumpManagerObject/New-ProcdumpManagerObject.ps1
@@ -0,0 +1,260 @@
+Function New-ProcdumpManagerObject {
+[CmdletBinding()]
+param(
+[string]$BinaryProcdumpExe = ".\procdump.exe",
+[string]$ProcessName, 
+[hashtable]$ProcessIDToServer,
+[string]$DumpFolder,
+[bool]$DumpWithMA = $true,
+[bool]$DumpWithMP = $false, 
+[int]$NumberOfDumps = 3,
+[int]$SecondsWait = 30, 
+[array]$Servers,
+[bool]$VerboseEnabled = $false,
+[scriptblock]$HostFunctionCaller,
+[scriptblock]$VerboseFunctionCaller
+)
+#This Script requires Invoke-Command to be able to be run, even locally. 
+#Function Version 1.0
+<# 
+#TODO List: 
+- Add Verbose information 
+- Add debug status info for dumping out the information when attemtping to execute procdump
+#>
+
+
+########## Parameter Binding Exceptions ##############
+# throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid ParameterName" 
+if([string]::IsNullOrEmpty($BinaryProcdumpExe))
+{
+    throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid BinaryProcdumpExe." 
+}
+if(([string]::IsNullOrEmpty($ProcessName) -and
+    ($ProcessIDToServer -eq $null)) -or 
+    (!([string]::IsNullOrEmpty($ProcessName)) -and 
+    ($ProcessIDToServer -ne $null)))
+{
+    throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid ProcessName or ProcessIDToServer. Only one must be used." 
+}
+if(!([string]::IsNullOrEmpty($ProcessName)) -and 
+    $ProcessName.EndsWith(".exe"))
+{
+    $ProcessName = $ProcessName.Replace(".exe","")
+}
+if([string]::IsNullOrEmpty($DumpFolder))
+{
+    throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid DumpFolder." 
+}
+if(!($DumpFolder.EndsWith("\")))
+{
+    $DumpFolder = $DumpFolder + "\"
+}
+if(($DumpWithMA -eq $true -and 
+    $DumpWithMP -eq $true) -or 
+    ($DumpWithMA -eq $false -and 
+    $DumpWithMP -eq $false))
+{
+    throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid dump type. Either DumpWithMA or DumpWithMP must be set to true." 
+}
+if($NumberOfDumps -eq $null -or 
+    $NumberOfDumps -lt 1)
+{
+    throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid NumberOfDumps. Must be value greater than or equal to 1." 
+}
+if($SecondsWait -eq $null -or 
+    $SecondsWait -lt 0)
+{
+    throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid SecondsWait. Must be a value greater than or equal to 0." 
+}
+if($Servers -eq $null -or 
+    $Servers.Count -lt 1)
+{
+    throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid BinaryProcdumpExe" 
+}
+
+Function Test-ValidProcdumpPath {
+    foreach($server in $Servers)
+    {
+        [bool]$result = Invoke-Command -ComputerName $server -ScriptBlock {param($passedArg); $test = Test-Path $passedArg; return $test } -ArgumentList $BinaryProcdumpExe 
+        if(!($result))
+        {
+            $throwString = "Failed to provide a valid path to procdump binary on server: {0}" -f $server
+            throw [System.Management.Automation.ParameterBindingException] $throwString 
+        }
+    }
+}
+
+Function Test-ValidDumpPath {
+    foreach($server in $Servers)
+    {
+        [bool]$result = Invoke-Command -ComputerName $server -ScriptBlock {param($passedArg); $test = Test-Path $passedArg; return $test} -ArgumentList $DumpFolder 
+        if(!($result))
+        {
+            $throwString = "Failed to provide a valid path to dump out the data to on server: {0}" -f $server
+            throw [System.Management.Automation.ParameterBindingException] $throwString 
+        }
+    }
+}
+
+Function Test-ValidHashTableKeys {
+    foreach($server in $Servers)
+    {
+        if(!($ProcessIDToServer.ContainsKey($server)))
+        {
+            $throwString = "Failed to provide a key of {0} in the ProcessIDToServer hashtable" -f $server
+            throw [System.Management.Automation.ParameterBindingException] $throwString 
+        }
+    }
+}
+
+Function Test-UsingProcessIDToServer {
+    if($ProcessIDToServer -eq $null)
+    {
+        return $false 
+    }
+    return $true 
+}
+
+Function Write-VerboseWriter {
+param(
+[Parameter(Mandatory=$true)][string]$WriteString
+)
+    if($this.VerboseFunctionCaller -eq $null -and 
+        $this.WriteVerboseData)
+    {
+        Write-Host $WriteString -ForegroundColor Cyan 
+    }
+    elseif($this.WriteVerboseData)
+    {
+        $this.VerboseFunctionCaller($WriteString)
+    }
+}
+        
+Function Write-HostWriter{
+param(
+[Parameter(Mandatory=$true)][string]$WriteString
+)
+    if($this.HostFunctionCaller -eq $null)
+    {
+        Write-Host $WriteString
+    }
+    else 
+    {
+        $this.HostFunctionCaller($WriteString)
+    }
+}
+
+Test-ValidProcdumpPath
+Test-ValidDumpPath
+if(Test-UsingProcessIDToServer)
+{
+    Test-ValidHashTableKeys
+}
+
+$procdumpManagerObject = New-Object PSCustomObject 
+
+$procdumpManagerObject | Add-Member -MemberType NoteProperty -Name "BinaryProcdumpExe" -Value $BinaryProcdumpExe
+$procdumpManagerObject | Add-Member -MemberType NoteProperty -Name "ProcessName" -Value $ProcessName
+$procdumpManagerObject | Add-Member -MemberType NoteProperty -Name "ProcessIDToServer" -Value $ProcessIDToServer
+$procdumpManagerObject | Add-Member -MemberType NoteProperty -Name "DumpFolder" -Value $DumpFolder 
+$procdumpManagerObject | Add-Member -MemberType NoteProperty -Name "DumpWithMA" -Value $DumpWithMA
+$procdumpManagerObject | Add-Member -MemberType NoteProperty -Name "NumberOfDumps" -Value $NumberOfDumps
+$procdumpManagerObject | Add-Member -MemberType NoteProperty -Name "SecondsWait" -Value $SecondsWait 
+$procdumpManagerObject | Add-Member -MemberType NoteProperty -Name "Servers" -Value $Servers
+$procdumpManagerObject | Add-Member -MemberType NoteProperty -Name "WriteVerboseData" -Value $VerboseEnabled 
+$procdumpManagerObject | Add-Member -MemberType NoteProperty -Name "UsingProcessIDToServer" -Value (Test-UsingProcessIDToServer)
+$procdumpManagerObject | Add-Member -MemberType ScriptMethod -Name "WriteVerboseWriter" -Value ${Function:Write-VerboseWriter}
+$procdumpManagerObject | Add-Member -MemberType ScriptMethod -Name "WriteHostWriter" -Value ${Function:Write-HostWriter}
+
+if($HostFunctionCaller -ne $null)
+{
+    $procdumpManagerObject | Add-Member -MemberType ScriptMethod -Name "HostFunctionCaller" -Value $HostFunctionCaller
+}
+if($VerboseFunctionCaller -ne $null)
+{
+    $procdumpManagerObject | Add-Member -MemberType ScriptMethod -Name "VerboseFunctionCaller" -Value $VerboseFunctionCaller
+}
+
+$procdumpManagerObject | Add-Member -MemberType ScriptMethod -Name "ValidateProcessOnServer" -Value {
+    if($this.UsingProcessIDToServer)
+    {
+        $processIdToServer = $this.ProcessIDToServer
+        foreach($key in $processIdToServer.Keys)
+        {
+            $processId = $processIdToServer[$key]
+            [bool]$result = Invoke-Command -ComputerName $key -ScriptBlock {param($passedArg); $getProcess = Get-Process -Id $passedArg -ErrorAction SilentlyContinue; if($getProcess -eq $null){return $false}; return $true } -ArgumentList $processId
+            if(!($result))
+            {
+                #for now do a return false: TODO: return a failure result as it is possible that process could change while we are monitoring. 
+                $writeHost = "Failed to get the process ID: {0} from Server: {1}" -f $processId, $key 
+                $this.WriteHostWriter($writeHost)
+                return $false
+            }
+        }
+        return $true 
+    }
+    else 
+    {
+        $processName = $this.ProcessName
+        foreach($server in $this.Servers)
+        {
+            [bool]$result = Invoke-Command -ComputerName $server -ScriptBlock {param($passedArg); $getProcess = Get-Process $passedArg -ErrorAction SilentlyContinue; if($getProcess -eq $null){return $false}; return $true } -ArgumentList $processName
+            if(!($result))
+            {
+                $writeHost = "Failed to find the process name of '{0}' on server {1}" -f $processName, $server
+                $this.WriteHostWriter($writeHost)
+                return $false 
+            }
+        }
+        return $true 
+    }
+}
+
+$procdumpManagerObject | Add-Member -MemberType ScriptMethod -Name "DumpOnServer" -Value {
+    param(
+    [string]$Server 
+    )
+    if([string]::IsNullOrEmpty($Server) -or 
+        (!($this.Servers.Contains($Server))))
+    {
+        throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid Server parameter"
+    }
+
+    if($this.UsingProcessIDToServer)
+    {
+        $processId = $this.ProcessIDToServer[$Server]
+    }
+    else 
+    {
+        $processId = $this.ProcessName 
+    }
+    if($this.DumpWithMA)
+    {
+        $dumpSwith = "-ma"
+    }
+    else 
+    {
+        $dumpSwith = "-mp"
+    }
+    $argList = New-Object PSCustomObject 
+    $argList | Add-Member -MemberType NoteProperty -Name "BinaryProcdumpExe" -Value $this.BinaryProcdumpExe
+    $argList | Add-Member -MemberType NoteProperty -Name "ProcessId" -Value $processId 
+    $argList | Add-Member -MemberType NoteProperty -Name "DumpSwitch" -Value $dumpSwith 
+    $argList | Add-Member -MemberType NoteProperty -Name "DumpFolder" -Value $this.DumpFolder
+    $argList | Add-Member -MemberType NoteProperty -Name "NumberOfDumps" -Value $this.NumberOfDumps 
+    $argList | Add-Member -MemberType NoteProperty -Name "Seconds" -Value $this.SecondsWait
+
+    Invoke-Command -ComputerName $Server -ScriptBlock {
+        param($passedArg) 
+        $procDump = $passedArg.BinaryProcdumpExe
+        $processId = $passedArg.ProcessId
+        $dumpSwith = $passedArg.DumpSwitch
+        $dumpFolder = $passedArg.DumpFolder 
+        $numberOfDumps = $passedArg.NumberOfDumps
+        $seconds = $passedArg.Seconds 
+        & $procDump $dumpSwith $processId $dumpFolder -n $numberOfDumps -s $seconds -accepteula
+    } -ArgumentList $argList 
+}
+
+return $procdumpManagerObject 
+}
---
+++ Functions/New-ProcdumpManagerObject/Tests/New-ProcdumpManagerObject.Tests.ps1
@@ -0,0 +1,64 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.','.'
+$here = $here.Replace("\Tests","")
+. "$here\$sut"
+
+#Load Required Functions
+
+$content = Get-Content "$here\$sut" 
+$foundRequiredFunctions = $false 
+foreach($line in $content)
+{
+    if($foundRequiredFunctions)
+    {
+        if($line.Contains("#>"))
+        {
+            break 
+        }
+        $webRequest = Invoke-WebRequest $line.Trim()
+        if($webRequest -ne $null -and $webRequest.Content)
+        {
+            Invoke-Expression $webRequest.Content 
+        }
+    }
+    if($line.Contains("Required Functions:"))
+    {
+        $foundRequiredFunctions = $true 
+        continue
+    }
+}
+
+$procdumpLocation = "{0}\{1}\procdump.exe" -f (Split-Path -Parent $MyInvocation.MyCommand.Path), "PesterTests.Ignore"
+$dumpFolder = $procdumpLocation.Replace("procdump.exe","") 
+$items = Get-ChildItem $dumpFolder | ?{$_.Name -like "*.dmp"}
+foreach($item in $items)
+{
+    Remove-Item $item -Force 
+}
+if(!(Test-Path $procdumpLocation))
+{
+    throw "failed to provide valid path to procdump location"
+    exit 
+}
+$processName = "explorer.exe" 
+$badProcessName = "badName" 
+
+Describe "Testing New-ProcdumpManagerObject" {
+
+    Context "Good Test Results" {
+        $procdumpObject = New-ProcdumpManagerObject -BinaryProcdumpExe $procdumpLocation -DumpFolder $dumpFolder -ProcessName $processName -Servers $env:COMPUTERNAME -SecondsWait 3 
+        It "Testing ValidateProcessOnServer"{
+            $procdumpObject.ValidateProcessOnServer() | Should Be $true 
+        }
+        It "Testing Dump" {
+            $dumpResults = $procdumpObject.DumpOnServer($env:COMPUTERNAME)
+            $dumpResults[-2].Contains("Dump count reached.") | Should Be $true 
+        }
+    }
+    Context "Bad Test Results" {
+        $procdumpObject = New-ProcdumpManagerObject -BinaryProcdumpExe $procdumpLocation -DumpFolder $dumpFolder -ProcessName $badProcessName -Servers $env:COMPUTERNAME -SecondsWait 3 
+        It "Testing Bad ValidateProcessOnServer" {
+            $procdumpObject.ValidateProcessOnServer() | Should Be $false 
+        }
+    }
+}
