c2600c18024dc5c29f662a544983aae1f194ad33
David Paulson <dpaul@microsoft.com>
2019-10-08 22:14:35 -0500

    Created Get-PerformanceThresholdCounters
    This is a function that can create a hashtable that gets returned that is in the structure required for New-PerformanceCounterMonitorThresholdObject

---
+++ Functions/Get-PerformanceThresholdCounters/Get-PerformanceThresholdCounters.ps1
@@ -0,0 +1,81 @@
+Function Get-PerformanceThresholdCounters {
+[CmdletBinding()]
+[OutputType([Hashtable])]
+param(
+[string]$PerformanceCounter,
+[array]$Servers = @($env:COMPUTERNAME),
+[double]$AverageThreshold,
+[double]$MaxThreshold,
+[string]$ThresholdType,
+[System.Management.Automation.ActionPreference]$ActionPreference = "SilentlyContinue"
+)
+#Function Version 1.0
+<# 
+Required Functions: 
+    https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-VerboseWriters/Write-VerboseWriter.ps1
+#>
+<#
+TODO: 
+- Add an instance filter option
+#>
+
+Write-VerboseWriter("Calling: Get-PerformanceThresholdCounters")
+Write-VerboseWriter("Passed: [string]PerformanceCounter: {0} | [double]AverageThreshold: {1} | [double]MaxThreshold: {2} | [string]ThresholdType: {3} | [ActionPreference]ActionPreference: {4}" -f `
+    $PerformanceCounter, 
+    $AverageThreshold, 
+    $MaxThreshold, 
+    $ThresholdType, 
+    $ActionPreference)
+Function Get-PerformanceThresholdCounterObject {
+    $obj = New-Object PSCustomObject 
+    $obj | Add-Member -MemberType NoteProperty -Name "AverageThreshold" -Value $AverageThreshold
+    $obj | Add-Member -MemberType NoteProperty -Name "MaxThreshold" -Value $MaxThreshold
+    $obj | Add-Member -MemberType NoteProperty -Name "ThresholdType" -Value $ThresholdType
+    return $obj 
+}
+
+########## Parameter Binding Exceptions ##############
+if([string]::IsNullOrEmpty($PerformanceCounter) -or
+    $PerformanceCounter.StartsWith("\"))
+{
+    throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid PerformanceCounter. Can not be null or empty and must not start with a '\'." 
+}
+if([string]::IsNullOrEmpty($ThresholdType) -or (
+    $ThresholdType -ne "LessThan" -and 
+    $ThresholdType -ne "GreaterThan"))
+{
+    throw [System.Management.Automation.ParameterBindingException] "Failed to provide valid ThresholdType. Accepted values are LessThan and GreaterThan." 
+}
+
+try 
+{
+    [System.Diagnostics.Stopwatch]$stopWatch = [System.Diagnostics.Stopwatch]::StartNew()
+    Write-VerboseWriter("Getting the counters....")
+    $counterSamples = (Get-Counter -ComputerName $Servers -Counter $PerformanceCounter -ErrorAction $ActionPreference ).CounterSamples
+    if($counterSamples.Count -eq 0)
+    {
+        throw "Failed to return any counters..." 
+    } 
+    Write-VerboseWriter("Finished getting the counters. Took {0} seconds to complete." -f ($stopWatch.Elapsed.TotalSeconds))
+}
+catch 
+{
+    $throwString = "Failed to run Get-Counter without any error. Error: '{0}'" -f ($Error[0].Exception)
+    throw  $throwString
+}
+
+$returnHash = @{}
+$currentSeconds = $stopWatch.Elapsed.TotalSeconds
+$counterPaths = ($counterSamples | Group-Object Path).Name
+Write-VerboseWriter("Took {0} seconds to group the counter paths." -f ($stopWatch.Elapsed.TotalSeconds - $currentSeconds))
+$currentSeconds = $stopWatch.Elapsed.TotalSeconds
+foreach($path in $counterPaths) 
+{
+    $returnHash.Add($path, (Get-PerformanceThresholdCounterObject))
+}
+$writeString = "Took {0} seconds to add {1} paths to the hash table." -f ($stopWatch.Elapsed.TotalSeconds - $currentSeconds), ($counterPaths.Count)
+Write-VerboseWriter($writeString)
+Write-VerboseWriter("Took {0} seconds to complete the Get-PerformanceThresholdCounters function" -f ($stopWatch.Elapsed.TotalSeconds))
+
+return $returnHash 
+}
---
+++ Functions/Get-PerformanceThresholdCounters/Tests/Get-PerformanceThresholdCounters.Tests.ps1
@@ -0,0 +1,67 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.','.'
+$here = $here.Replace("\Tests","")
+. "$here\$sut"
+
+#Load Required Functions
+
+$content = Get-Content "$here\$sut" 
+$foundRequiredFunctions = $false 
+foreach($line in $content)
+{
+    if($foundRequiredFunctions)
+    {
+        if($line.Contains("#>"))
+        {
+            break 
+        }
+        $webRequest = Invoke-WebRequest $line.Trim()
+        if($webRequest -ne $null -and $webRequest.Content)
+        {
+            Invoke-Expression $webRequest.Content 
+        }
+    }
+    if($line.Contains("Required Functions:"))
+    {
+        $foundRequiredFunctions = $true 
+        continue
+    }
+}
+
+$counter = "LogicalDisk(*)\*" 
+$thresholdTypes = @("GreaterThan","LessThan")
+$badThresholdTypes = @("GreaterThanOrEqualTo","LessThanOrEqualTo")
+$badCounter = "Blah\Counter" 
+
+Describe "Testing Get-PerformanceThresholdCounters" {
+    Context "Good Tests Results" {
+        $performanceThreshold = Get-PerformanceThresholdCounters -PerformanceCounter $counter -ThresholdType $thresholdTypes[0] 
+        IT "Testing Type" {
+            $performanceThreshold.GetType().Name | Should Be "HashTable" 
+        }
+        It "Testing Name and Counter" {
+            $testingName = "\\{0}\{1}(" -f ($env:COMPUTERNAME), ($counter.Substring(0, $counter.IndexOf("(")))
+            foreach($key in $performanceThreshold.Keys) 
+            {
+                $key.ToLower().StartsWith($testingName.ToLower()) | Should Be "True" 
+            }
+        }
+    }
+    Context "Throws" {
+        It "Bad Threshold Type" {
+            {Get-PerformanceThresholdCounters -PerformanceCounter $counter -ThresholdType $badThresholdTypes[0]} | Should -Throw `
+            "Failed to provide valid ThresholdType. Accepted values are LessThan and GreaterThan."
+        }
+        It "Bad Passed Counter Name" {
+            {Get-PerformanceThresholdCounters -PerformanceCounter ("\{0}" -f $counter) -ThresholdType $thresholdTypes[0] }| Should -Throw `
+            "Failed to provide valid PerformanceCounter. Can not be null or empty and must not start with a '\'."
+        }
+        It "Bad Counter on Get-Counter - Error Action SilentlyContinue" {
+            {Get-PerformanceThresholdCounters -PerformanceCounter $badCounter -ThresholdType $thresholdTypes[0]} | Should -Throw "Failed to return any counters..."
+        }
+        It "Bad Counter on Get-Counter - Error Action Stop" {
+            {Get-PerformanceThresholdCounters -PerformanceCounter $badCounter -ThresholdType $thresholdTypes[0] -ActionPreference "Stop"} | Should -Throw `
+            "Failed to run Get-Counter without any error. Error"
+        }
+    }
+}
