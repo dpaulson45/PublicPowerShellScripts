986cf15ae5a531dd8dac506647d95adb983a15d3
David Paulson <dpaul@microsoft.com>
2019-12-27 11:47:55 -0600

    Updated Compress-Folder to be used in a remote invoke
    Added the ability to do the function in a remote invoke and return all the debug information in a single variable.

--- Functions/Compress-Folder/Compress-Folder.ps1
+++ Functions/Compress-Folder/Compress-Folder.ps1
@@ -1,17 +1,18 @@
 Function Compress-Folder {
 [CmdletBinding()]
 param(
-[Parameter(Mandatory=$true)][string]$Folder,
+[Parameter(Mandatory=$false)][string]$Folder,
 [Parameter(Mandatory=$false)][bool]$IncludeMonthDay = $false,
 [Parameter(Mandatory=$false)][bool]$IncludeDisplayZipping = $true,
-[Parameter(Mandatory=$false)][bool]$ReturnCompressedLocation = $false
+[Parameter(Mandatory=$false)][bool]$ReturnCompressedLocation = $false,
+[Parameter(Mandatory=$false,Position=1)][object]$PassedObjectParameter
 )
 
-#Function Version 1.3
+#Function Version 1.4
 <#
 Required Functions:
-    https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-VerboseWriters/Write-VerboseWriter.ps1
-    https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-HostWriters/Write-HostWriter.ps1
+    https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-VerboseWriters/Write-InvokeCommandReturnVerboseWriter.ps1
+    https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-HostWriters/Write-InvokeCommandReturnHostWriter.ps1
 #>
 
 Function Get-DirectorySize {
@@ -19,8 +20,8 @@ param(
 [Parameter(Mandatory=$true)][string]$Directory,
 [Parameter(Mandatory=$false)][bool]$IsCompressed = $false 
 )
-    Write-VerboseWriter("Calling: Get-DirectorySize")
-    Write-VerboseWriter("Passed: [string]Directory: {0} | [bool]IsCompressed: {1}" -f $Directory, $IsCompressed)
+    Write-InvokeCommandReturnVerboseWriter("Calling: Get-DirectorySize") 
+    Write-InvokeCommandReturnVerboseWriter("Passed: [string]Directory: {0} | [bool]IsCompressed: {1}" -f $Directory, $IsCompressed)
     $itemSize = 0
     if($IsCompressed)
     {
@@ -39,22 +40,22 @@ param(
 Function Enable-IOCompression
 {
     $successful = $true 
-    Write-VerboseWriter("Calling: Enable-IOCompression")
+    Write-InvokeCommandReturnVerboseWriter("Calling: Enable-IOCompression")
     try 
     {
         Add-Type -AssemblyName System.IO.Compression.Filesystem -ErrorAction Stop
     }
     catch 
     {
-        Write-HostWriter("Failed to load .NET Compression assembly. Unable to compress up the data.")
+        Write-InvokeCommandReturnHostWriter("Failed to load .NET Compression assembly. Unable to compress up the data.")
         $successful = $false 
     }
-    Write-VerboseWriter("Returned: [bool]{0}" -f $successful)
+    Write-InvokeCommandReturnVerboseWriter("Returned: [bool]{0}" -f $successful)
     return $successful
 }
 Function Confirm-IOCompression 
 {
-    Write-VerboseWriter("Calling: Confirm-IOCompression")
+    Write-InvokeCommandReturnVerboseWriter("Calling: Confirm-IOCompression")
     $assemblies = [Appdomain]::CurrentDomain.GetAssemblies()
     $successful = $false
     foreach($assembly in $assemblies)
@@ -65,28 +66,27 @@ Function Confirm-IOCompression
             break 
         }
     }
-    Write-VerboseWriter("Returned: [bool]{0}" -f $successful)
+    Write-InvokeCommandReturnVerboseWriter("Returned: [bool]{0}" -f $successful)
     return $successful
 }
 
 Function Compress-Now
 {
-    Write-VerboseWriter("Calling: Compress-Now ")
+    Write-InvokeCommandReturnVerboseWriter("Calling: Compress-Now ")
     $zipFolder = Get-ZipFolderName -Folder $Folder -IncludeMonthDay $IncludeMonthDay
     if($IncludeDisplayZipping)
     {
-        Write-HostWriter("Compressing Folder {0}" -f $Folder)
+        Write-InvokeCommandReturnHostWriter("Compressing Folder {0}" -f $Folder)
     }
     $sizeBytesBefore = Get-DirectorySize -Directory $Folder
     $timer = [System.Diagnostics.Stopwatch]::StartNew()
     [System.IO.Compression.ZipFile]::CreateFromDirectory($Folder, $zipFolder)
     $timer.Stop()
     $sizeBytesAfter = Get-DirectorySize -Directory $zipFolder -IsCompressed $true
-    Write-VerboseWriter("Compressing directory size of {0} MB down to the size of {1} MB took {2} seconds." -f ($sizeBytesBefore / 1MB), ($sizeBytesAfter / 1MB),  $timer.Elapsed.TotalSeconds)
-    
+    Write-InvokeCommandReturnVerboseWriter("Compressing directory size of {0} MB down to the size of {1} MB took {2} seconds." -f ($sizeBytesBefore / 1MB), ($sizeBytesAfter / 1MB),  $timer.Elapsed.TotalSeconds)
     if((Test-Path -Path $zipFolder))
     {
-        Write-VerboseWriter("Compress successful, removing folder.")
+        Write-InvokeCommandReturnVerboseWriter("Compress successful, removing folder.")
         Remove-Item $Folder -Force -Recurse 
     }
     if($ReturnCompressedLocation)
@@ -100,8 +100,8 @@ param(
 [Parameter(Mandatory=$true)][string]$Folder,
 [Parameter(Mandatory=$false)][bool]$IncludeMonthDay = $false
 )
-    Write-VerboseWriter("Calling: Get-ZipFolderName")
-    Write-VerboseWriter("Passed - [string]Folder:{0} | [bool]IncludeMonthDay:{1}" -f $Folder, $IncludeMonthDay)
+    Write-InvokeCommandReturnVerboseWriter("Calling: Get-ZipFolderName")
+    Write-InvokeCommandReturnVerboseWriter("Passed - [string]Folder:{0} | [bool]IncludeMonthDay:{1}" -f $Folder, $IncludeMonthDay)
     if($IncludeMonthDay)
     {
         $zipFolderNoEXT = "{0}-{1}" -f $Folder, (Get-Date -Format Md)
@@ -110,7 +110,7 @@ param(
     {
         $zipFolderNoEXT = $Folder
     }
-    Write-VerboseWriter("[string]zipFolderNoEXT: {0}" -f $zipFolderNoEXT)
+    Write-InvokeCommandReturnVerboseWriter("[string]zipFolderNoEXT: {0}" -f $zipFolderNoEXT)
     $zipFolder = "{0}.zip" -f $zipFolderNoEXT
     if(Test-Path $zipFolder)
     {
@@ -120,13 +120,41 @@ param(
             $i++
         }while(Test-Path $zipFolder)
     }
-    Write-VerboseWriter("Returned: [string]zipFolder {0}" -f $zipFolder)
+    Write-InvokeCommandReturnVerboseWriter("Returned: [string]zipFolder {0}" -f $zipFolder)
     return $zipFolder
 }
 
-
-Write-VerboseWriter("Calling: Compress-Folder")
-Write-VerboseWriter("Passed - [string]Folder: {0} | [bool]IncludeDisplayZipping: {1} | [bool]ReturnCompressedLocation: {2}" -f $Folder, 
+$Script:stringArray = @()
+if($PassedObjectParameter -ne $null)
+{
+    if($PassedObjectParameter.Folder -ne $null)
+    {
+        $Folder = $PassedObjectParameter.Folder 
+        if($PassedObjectParameter.IncludeDisplayZipping -ne $null)
+        {
+            $IncludeDisplayZipping = $PassedObjectParameter.IncludeDisplayZipping
+        }
+        if($PassedObjectParameter.ReturnCompressedLocation -ne $null)
+        {
+            $ReturnCompressedLocation = $PassedObjectParameter.ReturnCompressedLocation
+        }
+        if($PassedObjectParameter.IncludeMonthDay -ne $null)
+        {
+            $IncludeMonthDay = $PassedObjectParameter.IncludeMonthDay
+        }
+    }
+    else 
+    {
+        $Folder = $PassedObjectParameter
+    }
+    $InvokeCommandReturnWriteArray = $true 
+}
+if($Folder.EndsWith("\"))
+{
+    $Folder = $Folder.TrimEnd("\")
+}
+Write-InvokeCommandReturnVerboseWriter("Calling: Compress-Folder")
+Write-InvokeCommandReturnVerboseWriter("Passed - [string]Folder: {0} | [bool]IncludeDisplayZipping: {1} | [bool]ReturnCompressedLocation: {2}" -f $Folder, 
 $IncludeDisplayZipping,
 $ReturnCompressedLocation)
 
@@ -145,18 +173,32 @@ if(Test-Path $Folder)
         }
         else
         {
-            Write-HostWriter("Unable to compress folder {0}" -f $Folder)
-            Write-VerboseWriter("Unable to enable IO compression on this system")
+            Write-InvokeCommandReturnHostWriter("Unable to compress folder {0}" -f $Folder)
+            Write-InvokeCommandReturnVerboseWriter("Unable to enable IO compression on this system")
         }
     }
 }
 else
 {
-    Write-HostWriter("Failed to find the folder {0}" -f $Folder)
+    Write-InvokeCommandReturnHostWriter("Failed to find the folder {0}" -f $Folder)
+}
+if($InvokeCommandReturnWriteArray)
+{
+    if($ReturnCompressedLocation)
+    {
+        Write-InvokeCommandReturnVerboseWriter("Returning: {0}" -f $compressedLocation)
+        $hashTable = @{"ReturnObject"=$compressedLocation}
+        $Script:stringArray += $hashTable
+        return $Script:stringArray
+    }
+    else 
+    {
+        return $Script:stringArray   
+    }
 }
 if($ReturnCompressedLocation)
 {
-    Write-VerboseWriter("Returning: {0}" -f $compressedLocation)
+    Write-InvokeCommandReturnVerboseWriter("Returning: {0}" -f $compressedLocation)
     return $compressedLocation
 }
 }
