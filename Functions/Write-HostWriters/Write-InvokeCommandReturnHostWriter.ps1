6598de94708a8b994829b014fc8681ff53023971
David Paulson <dpaul@microsoft.com>
2019-12-27 11:46:37 -0600

    Updated Write-InvokeCommandReturn*Write Functions
    Updated these functions to adjusted the scope setting to avoid random issues with data being added to the stringArray value.

--- Functions/Write-HostWriters/Write-InvokeCommandReturnHostWriter.ps1
+++ Functions/Write-HostWriters/Write-InvokeCommandReturnHostWriter.ps1
@@ -1,13 +1,12 @@
-#Function Version 1.0
+#Function Version 1.1
 Function Write-InvokeCommandReturnHostWriter {
 param(
-[Parameter(Mandatory=$true)][string]$WriteString,
-[int]$ScopeLevel = 1
+[Parameter(Mandatory=$true)][string]$WriteString
 )
     if($InvokeCommandReturnWriteArray)
     {
         $hashTable = @{"Host"=("[Remote Server: {0}] : {1}" -f $env:COMPUTERNAME, $WriteString)}
-        Set-Variable stringArray -Value ($stringArray += $hashTable) -Scope $ScopeLevel 
+        Set-Variable stringArray -Value ($Script:stringArray += $hashTable) -Scope Script
     }
     else 
     {
--- Functions/Write-VerboseWriters/Write-InvokeCommandReturnVerboseWriter.ps1
+++ Functions/Write-VerboseWriters/Write-InvokeCommandReturnVerboseWriter.ps1
@@ -1,13 +1,12 @@
-#Function Version 1.0
+#Function Version 1.1
 Function Write-InvokeCommandReturnVerboseWriter {
 param(
-[Parameter(Mandatory=$true)][string]$WriteString,
-[int]$ScopeLevel = 1
+[Parameter(Mandatory=$true)][string]$WriteString
 )
     if($InvokeCommandReturnWriteArray)
     {
         $hashTable = @{"Verbose"=("[Remote Server: {0}] : {1}" -f $env:COMPUTERNAME, $WriteString)}
-        Set-Variable stringArray -Value ($stringArray += $hashTable) -Scope $ScopeLevel 
+        Set-Variable stringArray -Value ($Script:stringArray += $hashTable) -Scope Script
     }
     else 
     {
