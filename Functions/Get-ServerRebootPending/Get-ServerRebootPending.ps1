0ff41b40eb6548e5be6b50aa5d536db0c1f9cebc
David Paulson <dpaul@microsoft.com>
2020-01-19 19:09:51 -0600

    Updated Get-ServerRebootPending to return all test results

--- Functions/Get-ServerRebootPending/Get-ServerRebootPending.ps1
+++ Functions/Get-ServerRebootPending/Get-ServerRebootPending.ps1
@@ -4,7 +4,7 @@ param(
 [Parameter(Mandatory=$true)][string]$ServerName,
 [Parameter(Mandatory=$false)][scriptblock]$CatchActionFunction
 )
-#Function Version 1.0
+#Function Version 1.1
 <# 
 Required Functions: 
     https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-VerboseWriters/Write-VerboseWriter.ps1
@@ -17,10 +17,7 @@ Function Get-PendingFileReboot {
         {
             return $true 
         }
-        else 
-        {
-            return $false 
-        }
+        return $false
     }
     catch 
     {
@@ -31,10 +28,7 @@ Function Get-PendingSCCMReboot {
     try 
     {
         $sccmReboot = Invoke-CimMethod -Namespace 'Root\ccm\clientSDK' -ClassName 'CCM_ClientUtilities' -Name 'DetermineIfRebootPending' -ErrorAction Stop 
-        if($sccmReboot -and ($sccmReboot.RebootPending -or $sccmReboot.IsHardRebootPending))
-        {
-            return $true 
-        }
+        return $sccmReboot
     }
     catch 
     {
@@ -56,26 +50,17 @@ param(
 }
 
 Write-VerboseWriter("Calling: Get-ServerRebootPending")
-if(Invoke-ScriptBlockHandler -ComputerName $ServerName -ScriptBlock ${Function:Get-PendingFileReboot} -ScriptBlockDescription "Get-PendingFileReboot" -CatchActionFunction $CatchActionFunction)
+$pendingFileRenameOperationValue = Invoke-ScriptBlockHandler -ComputerName $ServerName -ScriptBlock ${Function:Get-PendingFileReboot} -ScriptBlockDescription "Get-PendingFileReboot" -CatchActionFunction $CatchActionFunction
+if($pendingFileRenameOperationValue -eq $null)
 {
-    Write-VerboseWriter("Get-PendingFileReboot Determined Reboot is pending. Registry HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\ item properties of PendingFileRenameOperations.")
-    return $true 
+    $pendingFileRenameOperationValue = $false
 }
-if(Invoke-ScriptBlockHandler -ComputerName $ServerName -ScriptBlock ${Function:Get-PendingSCCMReboot} -ScriptBlockDescription "Get-PendingSCCMReboot" -CatchActionFunction $CatchActionFunction)
-{
-    Write-VerboseWriter("Get-PendingSCCMReboot determined reboot is pending.")
-    return $true 
-}
-if(Invoke-ScriptBlockHandler -ComputerName $ServerName -ScriptBlock ${Function:Get-PathTestingReboot} -ScriptBlockDescription "Get-PendingAutoUpdateReboot" -CatchActionFunction $CatchActionFunction -ArgumentList "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending")
-{
-    Write-VerboseWriter("Get-PathTestingReboot for HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending determined reboot is pending")
-    return $true 
-}
-if(Invoke-ScriptBlockHandler -ComputerName $ServerName -ScriptBlock ${Function:Get-PathTestingReboot} -ScriptBlockDescription "Get-PendingAutoUpdateReboot" -CatchActionFunction $CatchActionFunction -ArgumentList "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired")
-{
-    Write-VerboseWriter("Get-PathTestingReboot for HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired determined reboot is pending")
-    return $true 
-}
-Write-VerboseWriter("Passed all reboot checks.")
-return $false 
+$serverPendingReboot = New-Object PSCustomObject
+$serverPendingReboot | Add-Member -MemberType NoteProperty -Name "PendingFileRenameOperations" -Value $pendingFileRenameOperationValue
+$serverPendingReboot | Add-Member -MemberType NoteProperty -Name "SccmReboot" -Value (Invoke-ScriptBlockHandler -ComputerName $ServerName -ScriptBlock ${Function:Get-PendingSCCMReboot} -ScriptBlockDescription "Get-PendingSCCMReboot" -CatchActionFunction $CatchActionFunction)
+$serverPendingReboot | Add-Member -MemberType NoteProperty -Name "ComponentBasedServicingPendingReboot" -Value (Invoke-ScriptBlockHandler -ComputerName $ServerName -ScriptBlock ${Function:Get-PathTestingReboot} -ScriptBlockDescription "Get-PendingAutoUpdateReboot" -CatchActionFunction $CatchActionFunction -ArgumentList "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending")
+$serverPendingReboot | Add-Member -MemberType NoteProperty -Name "AutoUpdatePendingReboot" -Value (Invoke-ScriptBlockHandler -ComputerName $ServerName -ScriptBlock ${Function:Get-PathTestingReboot} -ScriptBlockDescription "Get-PendingAutoUpdateReboot" -CatchActionFunction $CatchActionFunction -ArgumentList "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired")
+$serverPendingReboot | Add-Member -MemberType NoteProperty -Name "SccmRebootPending" -Value ($serverPendingReboot.SccmReboot -and ($serverPendingReboot.SccmReboot.RebootPending -or $serverPendingReboot.SccmReboot.IsHardRebootPending))
+$serverPendingReboot | Add-Member -MemberType NoteProperty -Name "PendingReboot" -Value ($serverPendingReboot.PendingFileRenameOperations -or $serverPendingReboot.ComponentBasedServicingPendingReboot -or $serverPendingReboot.AutoUpdatePendingReboot -or $serverPendingReboot.SccmRebootPending)
+return $serverPendingReboot 
 }
