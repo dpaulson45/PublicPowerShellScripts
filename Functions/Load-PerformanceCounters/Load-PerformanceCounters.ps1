3aa1af049cba97e05c3a2fa0ae5bec1ce4a5c248
David Paulson <dpaul@microsoft.com>
2020-04-15 21:45:59 -0500

    Updated Load-PerformanceCounters to include faster Grouping option
    Using a different grouping option to improve performance

--- Functions/Load-PerformanceCounters/Load-PerformanceCounters.ps1
+++ Functions/Load-PerformanceCounters/Load-PerformanceCounters.ps1
@@ -9,12 +9,13 @@ param(
 [Parameter(Mandatory=$false)][array]$Counters
 )
 
-#Function Version 1.3
+#Function Version 1.4
 <# 
 Required Functions: 
     https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-VerboseWriters/Write-VerboseWriter.ps1
     https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/New-CounterDataObject/New-CounterDataObject.ps1
     https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Import-PerformanceCounters/Import-PerformanceCounters.ps1
+    https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Group-ObjectHashTable/Group-ObjectHashTable.ps1
 #>
 if($PSBoundParameters.ContainsKey('Directory'))
 {
@@ -29,14 +30,14 @@ foreach($filePath in $FilePaths)
 }
 [System.Diagnostics.Stopwatch]$stopWatchTotal = [System.Diagnostics.Stopwatch]::StartNew()
 $importCounters = Import-PerformanceCounters -FilePaths $FilePaths -MaxSample $MaxSample -StartTime $StartTime -EndTime $EndTime -Counters $Counters 
-$measureGroupTime = Measure-Command { $groupData = $importCounters | Group-Object Path }
+$measureGroupTime = Measure-Command { $hashtableGroup = $importCounters | Group-ObjectHashTable Path }
 Write-VerboseWriter("[{0}]: Took {1} seconds to group the path data" -f [datetime]::Now, $measureGroupTime.TotalSeconds)
 [System.Diagnostics.Stopwatch]$stopWatch = [System.Diagnostics.Stopwatch]::StartNew()
 $hashtable = @{}
-foreach($counterData in $groupData)
+foreach($key in $hashtableGroup.Keys)
 {
-    $counterObj =  New-CounterDataObject -PerformanceCounterSamples $counterData.Group 
-    $hashtable.Add($counterObj.FullName, $counterObj)
+    $counterObj =  New-CounterDataObject -PerformanceCounterSamples $hashtableGroup[$key]
+    $hashtable.Add($key, $counterObj)
 }
 Write-VerboseWriter("[{0}]: Additional time taken to add TimeHash {1} seconds" -f [datetime]::Now, $secondsTimeHash)
 Write-VerboseWriter("[{0}]: Finished building objects off the data. Completed in {1} seconds" -f [datetime]::Now, $stopWatch.Elapsed.TotalSeconds)
