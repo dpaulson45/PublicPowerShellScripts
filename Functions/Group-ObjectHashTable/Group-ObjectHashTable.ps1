a43e29ee99f22044bcb784e8af02317ee5dcae53
David Paulson <dpaul@microsoft.com>
2020-04-15 21:44:42 -0500

    Created Group-ObjectHashTable

---
+++ Functions/Group-ObjectHashTable/Group-ObjectHashTable.ps1
@@ -0,0 +1,38 @@
+Function Group-ObjectHashTable {
+param
+(
+  [string[]]
+  $Property
+)
+#Function Version 1.0
+#Found from: http://powershell.one/tricks/performance/group-object
+begin
+{
+  # create an empty hashtable
+  $hashtable = @{}
+}
+
+process
+{
+    # create a key based on the submitted properties, and turn
+    # it into a string
+  $key = $(foreach($prop in $Property) { $_.$prop }) -join ','
+      
+  # check to see if the key is present already
+  if ($hashtable.ContainsKey($key) -eq $false)
+  {
+    # add an empty array list 
+    $hashtable[$key] = [Collections.Arraylist]@()
+  }
+
+  # add element to appropriate array list:
+  $null = $hashtable[$key].Add($_)
+}
+
+end
+{
+  # return the entire hashtable:
+  $hashtable
+}
+
+}
---
+++ Functions/Group-ObjectHashTable/README.md
@@ -0,0 +1,4 @@
+# Group-ObjectHashTable
+Function that is used to replace Group-Object as it performs better.
+
+Source: http://powershell.one/tricks/performance/group-object
