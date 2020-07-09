112db187aad89f4b35a84935f15a30cee0e670b5
David Paulson <dpaul@microsoft.com>
2020-07-08 23:03:43 -0500

    Updated Invoke-RegistryGetValue ReturnAfterOpenSubKey
    You can now return after OpenSubKey()
    Updated README

--- Functions/Invoke-RegistryGetValue/Invoke-RegistryGetValue.ps1
+++ Functions/Invoke-RegistryGetValue/Invoke-RegistryGetValue.ps1
@@ -5,38 +5,50 @@ param(
 [Parameter(Mandatory=$true)][string]$MachineName,
 [Parameter(Mandatory=$true)][string]$SubKey,
 [Parameter(Mandatory=$true)][string]$GetValue,
+[Parameter(Mandatory=$false)][bool]$ReturnAfterOpenSubKey,
 [Parameter(Mandatory=$false)][object]$DefaultValue,
 [Parameter(Mandatory=$false)][scriptblock]$CatchActionFunction
 )
 
-#Function Version 1.1
-<# 
-Required Functions: 
+#Function Version 1.2
+<#
+Required Functions:
     https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-VerboseWriters/Write-VerboseWriter.ps1
 #>
-Write-VerboseWriter("Calling: Invoke-RegistryHandler")
-try 
+Write-VerboseWriter("Calling: Invoke-RegistryGetValue")
+try
 {
     Write-VerboseWriter("Attempting to open the Base Key '{0}' on Server '{1}'" -f $RegistryHive, $MachineName)
     $Reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($RegistryHive, $MachineName)
     Write-VerboseWriter("Attempting to open the Sub Key '{0}'" -f $SubKey)
     $RegKey= $Reg.OpenSubKey($SubKey)
+
+    if ($ReturnAfterOpenSubKey)
+    {
+        Write-VerboseWriter("Returning OpenSubKey")
+        return $RegKey
+    }
+
     Write-VerboseWriter("Attempting to get the value '{0}'" -f $GetValue)
     $returnGetValue = $RegKey.GetValue($GetValue)
-    if($returnGetValue -eq $null -and $DefaultValue -ne $null)
+
+    if ($null -eq $returnGetValue -and
+        $null -ne $DefaultValue)
     {
         Write-VerboseWriter("No value found in the registry. Setting to default value: {0}" -f $DefaultValue)
         $returnGetValue = $DefaultValue
     }
+
     Write-VerboseWriter("Exiting: Invoke-RegistryHandler | Returning: {0}" -f $returnGetValue)
     return $returnGetValue
 }
-catch 
+catch
 {
-    if($CatchActionFunction -ne $null)
+    if ($CatchActionFunction -ne $null)
     {
         & $CatchActionFunction
     }
+
     Write-VerboseWriter("Failed to open the registry")
 }
 
