ee8f9b5f93540979bb07f7a3765b25d2188f06f9
David Paulson <dpaul@microsoft.com>
2020-10-25 21:49:11 -0500

    Updated OS Version detection to use caption vs build number

--- Functions/Get-ServerOperatingSystemVersion/Get-ServerOperatingSystemVersion.ps1
+++ Functions/Get-ServerOperatingSystemVersion/Get-ServerOperatingSystemVersion.ps1
@@ -1,39 +1,41 @@
 Function Get-ServerOperatingSystemVersion {
 [CmdletBinding()]
 param(
-[string]$OSBuildNumberVersion
+[string]$OsCaption
 )
 
-#Function Version 1.4
+#Function Version 1.5
 <#
 Required Functions: 
     https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-VerboseWriters/Write-VerboseWriter.ps1
 #>
 
-if($OSBuildNumberVersion -eq [string]::Empty -or $OSBuildNumberVersion -eq $null)
+if($OsCaption -eq [string]::Empty -or
+    $OsCaption -eq $null)
 {
     Write-VerboseWriter("Getting the local machine version build number")
-    $OSBuildNumberVersion = (Get-WmiObject -Class Win32_OperatingSystem).Version
-    Write-VerboseWriter("Got {0} for the version build number" -f $OSBuildNumberVersion)
+    $OsCaption = (Get-WmiObject -Class Win32_OperatingSystem).Caption
+    Write-VerboseWriter("Got '{0}' for the caption" -f $OsCaption)
 }
 else 
 {
-    Write-VerboseWriter("Passed - [string]OSBuildNumberVersion : {0}" -f $OSBuildNumberVersion)
+    Write-VerboseWriter("Passed - [string]OsCaption : {0}" -f $OsCaption)
 }
 
-[string]$osReturnValue = ""
-switch ($OSBuildNumberVersion) 
+$osReturnValue = [string]::Empty
+
+switch -Wildcard ($OsCaption) 
 {
-    "6.0.6000" {$osReturnValue = "Windows2008"}
-    "6.1.7600" {$osReturnValue = "Windows2008R2"}
-    "6.1.7601" {$osReturnValue = "Windows2008R2"}
-    "6.2.9200" {$osReturnValue = "Windows2012"}
-    "6.3.9600" {$osReturnValue = "Windows2012R2"}
-    "10.0.14393" {$osReturnValue = "Windows2016"}
-    "10.0.17713" {$osReturnValue = "Windows2019"}
+    "*Server 2008 R2*" {$osReturnValue = "Windows2008R2"; break}
+    "*Server 2008*" {$osReturnValue = "Windows2008"}
+    "*Server 2012 R2*" {$osReturnValue = "Windows2012R2"; break}
+    "*Server 2012*" {$osReturnValue = "Windows2012"}
+    "*Server 2016*" {$osReturnValue = "Windows2016"}
+    "*Server 2019*" {$osReturnValue = "Windows2019"}
     default {$osReturnValue = "Unknown"}
 }
 
 Write-VerboseWriter("Returned: {0}" -f $osReturnValue)
 return [string]$osReturnValue
+
 }
