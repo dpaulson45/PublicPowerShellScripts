d69a6f54c3719a30a1f2d6090b05442c58699d93
Lukas Sassl <lusassl@microsoft.com>
2021-01-22 17:17:15 +0100

    Improvements to better run against local system

--- Functions/Common/Invoke-ScriptBlockHandler/Invoke-ScriptBlockHandler.ps1
+++ Functions/Common/Invoke-ScriptBlockHandler/Invoke-ScriptBlockHandler.ps1
@@ -18,7 +18,7 @@ Function Invoke-ScriptBlockHandler {
         Write-VerboseWriter($ScriptBlockDescription)
     }
     try {
-        if ($ComputerName -ne $env:COMPUTERNAME) {
+        if (($ComputerName).Split(".")[0] -ne $env:COMPUTERNAME) {
             $params = @{
                 ComputerName = $ComputerName
                 ScriptBlock  = $ScriptBlock
